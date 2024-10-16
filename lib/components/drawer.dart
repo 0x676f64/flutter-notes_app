988d2810dc3ee5d62b523f3e029f4d1d5ae970bf
Joey Rodriguez <oscardiggz@protonmail.com>
2024-10-16 16:41:23 -0500

    Create drawer.dart

---
+++ lib/components/drawer.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        children: [
+        // header
+        const DrawerHeader(
+          child: Icon(Icons.note),
+        ),
+          // note title
+          DrawerTitle(
+            title: 'Notes',
+            leading: const Icon(Icons.home),
+            onTap: () => Navigator.pop(context),
+          ),
+          // setting title
+          DrawerTitle(
+            title: "Settings",
+            leading: const Icon(Icons.settings),
+            onTap: () {},
+          )
+        ],
+      ),
+
+    );
+  }
+}
