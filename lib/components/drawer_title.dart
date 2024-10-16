0291f03045a86d7531a9093228fd15de905222d0
Joey Rodriguez <oscardiggz@protonmail.com>
2024-10-16 16:51:14 -0500

    Update drawer_title.dart

--- lib/components/drawer_title.dart
+++ lib/components/drawer_title.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class DrawerTitle extends StatelessWidget {
+class DrawerTile extends StatelessWidget {
   final String title;
   final Widget leading;
   final void Function()? onTap;
 
-  const DrawerTitle({
+  const DrawerTile({
     super.key,
     required this.title,
     required this.leading,
@@ -16,7 +16,7 @@ class DrawerTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0),
-      child: ListTitle(
+      child: ListTile(
       title: Text(
         title,
         style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
